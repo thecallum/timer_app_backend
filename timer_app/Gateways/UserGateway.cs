19add948e3556c485e0a8cf1c839439db6f95add
Callum Macpherson <callummac@protonmail.com>
2024-02-15 09:22:27 +0000

    Add logging etc

--- timer_app/Gateways/UserGateway.cs
+++ timer_app/Gateways/UserGateway.cs
@@ -8,16 +8,35 @@ namespace timer_app.Gateways
 {
     public class UserGateway : IUserGateway
     {
+        private readonly ILogger<IUserGateway> _logger;
+
+        public UserGateway(ILogger<IUserGateway> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task<Auth0User> GetUserData(string accessToken)
         {
+            _logger.LogInformation("UserGateway about to fetch user data");
+
             var domain = Environment.GetEnvironmentVariable("Auth0_Domain");
 
-            var client = new RestClient(domain);
+            var options = new RestClientOptions
+            {
+                BaseUrl = new Uri(domain),
+                ThrowOnAnyError = true
+            };
+
+            var client = new RestClient(options);
             var request = new RestRequest("/userinfo", Method.Get);
 
             request.AddHeader("Authorization", accessToken);
 
+            _logger.LogInformation("UserGateway is now fetching user data");
+
             var response = await client.ExecuteAsync(request);
+
+            _logger.LogInformation("UserGateway fetched user data with {Response} {Status}", response.Content, response.StatusCode);
 
             if (response.StatusCode != HttpStatusCode.OK)
             {
--- timer_app/Middleware/UserMiddleware.cs
+++ timer_app/Middleware/UserMiddleware.cs
@@ -6,14 +6,18 @@ namespace timer_app.Middleware
     public class UserMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<UserMiddleware> _logger;
 
-        public UserMiddleware(RequestDelegate next)
+        public UserMiddleware(RequestDelegate next, ILogger<UserMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext httpContext, IUserService userService)
         {
+            _logger.LogInformation("Invoking UserMiddlware");
+
             var accessToken = httpContext.Request.Headers["Authorization"].ToString();
 
             await userService.InitialiseUser(accessToken);
