34e4c6d3994bda3c745b63ee25955ec64c17f5a5
Callum Macpherson <callummac@protonmail.com>
2024-02-15 08:49:30 +0000

    Remove auth endpoint to nextjs

--- timer_app/Gateways/UserGateway.cs
+++ timer_app/Gateways/UserGateway.cs
@@ -8,13 +8,6 @@ namespace timer_app.Gateways
 {
     public class UserGateway : IUserGateway
     {
-        private readonly ILogger<IUserGateway> _logger;
-
-        public UserGateway(ILogger<IUserGateway> logger)
-        {
-            _logger = logger;
-        }
-
         public async Task<Auth0User> GetUserData(string accessToken)
         {
             var domain = Environment.GetEnvironmentVariable("Auth0_Domain");
@@ -39,46 +32,6 @@ namespace timer_app.Gateways
                 Name = responseData["name"].ToString(),
                 Email = responseData["email"].ToString(),
             };
-        }
-
-        public async Task<string> AuthorizeUser(string code)
-        {
-            _logger.LogInformation("Calling UserGateway.AuthorizeUser");
-
-            var domain = Environment.GetEnvironmentVariable("Auth0_Domain");
-            var clientId = Environment.GetEnvironmentVariable("Auth0_ClientId");
-            var clientSecret = Environment.GetEnvironmentVariable("Auth0_ClientSecret");
-            var redirect_uri = Environment.GetEnvironmentVariable("Auth0_RedirectUri");
-
-            _logger.LogInformation("Setting up request in UserGateway.AuthorizeUser");
-
-            var options = new RestClientOptions
-            {
-                BaseUrl = new Uri(domain),
-                ThrowOnAnyError = true
-            };
-
-            var client = new RestClient(options);
-            var request = new RestRequest("/oauth/token", Method.Post);
-
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("application/x-www-form-urlencoded", $"grant_type=authorization_code&client_id={clientId}&client_secret={clientSecret}&code={code}&redirect_uri={redirect_uri}", ParameterType.RequestBody);
-
-            var response = await client.ExecuteAsync(request);
-
-            _logger.LogInformation("Response recieved {Response} in UserGateway.AuthorizeUser with {StatusCode}", response.Content, response.StatusCode);
-
-            if (response.StatusCode == HttpStatusCode.Unauthorized)
-            {
-                throw new UnauthorizedAccessException(response.ErrorMessage);
-            }
-
-            // set cookie
-            var responseData = JObject.Parse(response.Content);
-
-            var accessToken = responseData["access_token"].ToString();
-
-            return accessToken;
         }
     }
 }
