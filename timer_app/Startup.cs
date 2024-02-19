b35f90ad7c3a8c7cfb026b85d9f196fc756e5745
Callum Macpherson <callummac@protonmail.com>
2024-02-19 07:14:01 +0000

    test

--- timer_app/Startup.cs
+++ timer_app/Startup.cs
@@ -77,15 +77,15 @@ public class Startup
                 options.IncludeErrorDetails = true;
                 //options.
 
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    //  ValidateIssuer = true,
-                    // ValidateAudience = true,
-                    ValidateLifetime = true,
-                    //  ValidateIssuerSigningKey = true,
-                    ClockSkew = TimeSpan.Zero,
+                //options.TokenValidationParameters = new TokenValidationParameters
+                //{
+                //    //  ValidateIssuer = true,
+                //    // ValidateAudience = true,
+                //    ValidateLifetime = true,
+                //    //  ValidateIssuerSigningKey = true,
+                //    ClockSkew = TimeSpan.Zero,
 
-                };
+                //};
             });
         }
     }
